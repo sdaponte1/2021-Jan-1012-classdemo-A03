428efffc04ad319160050b569841ab7b8b34c979
sdaponte1 <77166222+sdaponte1@users.noreply.github.com>
2021-01-15 11:54:43 -0700

    Exercise 01

---
+++ Exercise01_SDaPonte_Solution/Exercise01_SDaPonte/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise01_SDaPonte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Exercise01
+             * describing myself ( ͡° ͜ʖ ͡°)
+             * 15 Jan 2021
+            */
+
+            Console.WriteLine("Hello there, my name is Simao da Ponte, and I am currently in Computer " +
+               "Software Development DMIT.\n");
+
+            Console.WriteLine("Did you know I do enjoy a good session of minecraft with my buds?\n");
+
+
+            Console.WriteLine("As for my goal in this course, its 80% or higher, " +
+                "if not i will jut be glad to pass.\n");
+
+
+        }
+    }
+}
